adbb20e6682425217566f1d107d2727c476a9c7b
NeilJudson <shuiyoufeng@yeah.net>
2016-08-14 19:26:04 +0800

    harib01g
    添加绘制纯色方块的函数

--- future01/bootpack.c
+++ future01/bootpack.c
@@ -6,18 +6,36 @@ void io_store_eflags(int eflags);
 
 void init_palette(void);
 void set_palette(int start, int end, unsigned char *rgb);
+void boxfill8(unsigned char *vram, int xsize, unsigned char c, int x0, int y0, int x1, int y1);
+
+#define COL8_000000		0
+#define COL8_FF0000		1
+#define COL8_00FF00		2
+#define COL8_FFFF00		3
+#define COL8_0000FF		4
+#define COL8_FF00FF		5
+#define COL8_00FFFF		6
+#define COL8_FFFFFF		7
+#define COL8_C6C6C6		8
+#define COL8_840000		9
+#define COL8_008400		10
+#define COL8_848400		11
+#define COL8_000084		12
+#define COL8_840084		13
+#define COL8_008484		14
+#define COL8_848484		15
+
 void HariMain(void)
 {
-	int i;
 	char *p;
 
 	init_palette(); /* 设定调色板 */
 
 	p = (char *) 0xa0000;
 
-	for (i = 0; i <= 0xffff; i++) {
-		*(p + i) = i & 0x0f;
-	}
+	boxfill8(p, 320, COL8_FF0000,  20,  20, 120, 120);
+	boxfill8(p, 320, COL8_00FF00,  70,  50, 170, 150);
+	boxfill8(p, 320, COL8_0000FF, 120,  80, 220, 180);
 
 	for (;;) {
 		io_hlt();
@@ -55,7 +73,7 @@ void set_palette(int start, int end, unsigned char *rgb)
 	int i, eflags;
 	eflags = io_load_eflags();	/* 记录中断许可标志的值 */
 	io_cli(); 					/* 将中断许可标志置为0，禁止中断 */
-	io_out8(0x03c8, start);
+	io_out8(0x03c8, start);		/* 将想要设定的调色板号码写入0x03c8 */
 	for (i = start; i <= end; i++) {
 		io_out8(0x03c9, rgb[0] / 4);
 		io_out8(0x03c9, rgb[1] / 4);
@@ -63,5 +81,15 @@ void set_palette(int start, int end, unsigned char *rgb)
 		rgb += 3;
 	}
 	io_store_eflags(eflags);	/* 复原中断许可标志 */
+	return;
+}
+
+void boxfill8(unsigned char *vram, int xsize, unsigned char c, int x0, int y0, int x1, int y1)
+{
+	int x, y;
+	for (y = y0; y <= y1; y++) {
+		for (x = x0; x <= x1; x++)
+			vram[y * xsize + x] = c;
+	}
 	return;
 }
