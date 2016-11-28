2bd38630895a466280074c453621aad9698288a0
NeilJudson <shuiyoufeng@yeah.net>
2016-11-28 22:39:27 +0800

    future11c 测试性能
    future11c 测试性能

---
+++ future11_v2.0/mouse.h
@@ -0,0 +1,15 @@
+#include "fifo.h"
+
+#ifndef MOUSEH
+#define MOUSEH
+
+struct MOUSE_DEC {
+	unsigned char buf[3], phase;
+	int x, y, btn;
+};
+
+void inthandler2c(int *esp);
+void enable_mouse(struct FIFO32 *fifo, int data0, struct MOUSE_DEC *mdec);
+int mouse_decode(struct MOUSE_DEC *mdec, unsigned char dat);
+
+#endif
