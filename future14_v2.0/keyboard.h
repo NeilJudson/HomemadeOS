9bba3a44970b740f64a5609c610930df74c4f416
NeilJudson <shuiyoufeng@yeah.net>
2017-05-20 23:50:39 +0800

    future14a 闲置任务

---
+++ future14_v2.0/keyboard.h
@@ -0,0 +1,13 @@
+#include "fifo.h"
+
+#ifndef KEYBOARD_H
+#define KEYBOARD_H
+
+#define PORT_KEYDAT		0x0060
+#define PORT_KEYCMD		0x0064
+
+void inthandler21(int *esp);
+void wait_KBC_sendready(void);
+void init_keyboard(struct FIFO32 *fifo, int data0);
+
+#endif
